6534ae8e130b5317b0d7110113b55a88c9b3574c
TOKB27 <152061606+TOKB27@users.noreply.github.com>
2026-04-14 08:00:58 +0900

    tableActions.js を更新

--- application/assets/js/modules/tableActions.js
+++ application/assets/js/modules/tableActions.js
@@ -1,29 +1,19 @@
 // assets/js/modules/tableActions.js
 window.App = window.App || {};
 
-window.App.bindTableActions = function (modalWizard, detailView) {
+window.App.bindApplicationTableActions = function (modalWizard, detailView) {
   $("#btnNew").on("click", () => {
     modalWizard.open({ applyType: "NEW", record: null });
   });
 
   $("#recordsTable").on("click", ".btnDetail", function () {
-    const record = readRecordFromRow(this);
-    detailView.open({ record, modalWizard });
-  });
-
-  $("#recordsTable").on("click", ".btnEdit", function () {
-    const record = readRecordFromRow(this);
-    modalWizard.open({ applyType: "UPDATE", record });
-  });
-
-  $("#recordsTable").on("click", ".btnDelete", function () {
-    const record = readRecordFromRow(this);
-    modalWizard.open({ applyType: "DELETE", record });
+    const application = readApplicationFromRow(this);
+    detailView.open({ application });
   });
 };
 
-function readRecordFromRow(btn) {
-  const json = $(btn).closest("tr").attr("data-record");
+function readApplicationFromRow(btn) {
+  const json = $(btn).closest("tr").attr("data-application");
   try {
     return JSON.parse(json);
   } catch {
